5ca67ec301011a57022a7068e14a11d6b746cb80
panditdhamdhere <dhamdherepandit@gmail.com>
2023-06-14 09:47:01 +0530

    button added

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -4,10 +4,8 @@ import { useRouter } from "next/navigation";
 
 import { ShowMoreProps } from "@/types";
 import { updateSearchParams } from "@/utils";
-
-// import { updateSearchParams } from "@utils";
-// import { CustomButton } from "@components";
 import { CustomButton } from ".";
+
 
 const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
   const router = useRouter();
